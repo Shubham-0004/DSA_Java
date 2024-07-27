ba63d796242e2f9841956453b36e33bec5f06749
Shubham-0004 <itsmeshubham2904@gmail.com>
2024-07-27 14:27:50 +0530

    added files

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,5 +1,21 @@
-package DSA_Java.Sorting;cd
+import java.util.*;
 
 public class BubbleSort {
-    
+    public static void SortedArray(int numbers[]){
+        for (int i = 0; i < numbers.length-1; i++) {
+            for (int j = 0; j < numbers.length-i-1; j++) {
+                if (numbers[j]<numbers[j+1]) {  // for descending order
+                    int temp=numbers[j];
+                    numbers[j]=numbers[j+1];
+                    numbers[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array :"+Arrays.toString(numbers));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numbers[]={5,4,1,3,2};
+        SortedArray(numbers);
+    }
 }
