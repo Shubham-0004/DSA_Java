d5a0d440fb26e77508ccda6628b9a0001837785a
Shubham-0004 <itsmeshubham2904@gmail.com>
2024-07-27 11:52:13 +0530

    Buy and Sell Stocks

---
+++ Array/BuySellStocks.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class BuySellStocks {
+    public static int maxProfit(int prices[]){
+        int buyPrice=0;
+        int maxProfit=0;
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice<prices[i]) {  
+                //profit
+                int Profit=prices[i]-buyPrice; //ith day profit
+                maxProfit=Math.max(maxProfit, Profit);                
+            }
+            else{
+                buyPrice=prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        // creating the array for the prices of Stocks
+        int prices[]={7,1,5,3,6,4};
+        // calling the function 
+        int result=maxProfit(prices);
+
+        System.out.println("The maximum profit earned can be "+result);
+    }
+}
