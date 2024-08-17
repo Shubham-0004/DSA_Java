10ce3117e349cb29b7ba22a5bc7021c3227ea789
Shubham-0004 <itsmeshubham2904@gmail.com>
2024-08-17 20:22:23 +0530

    Added

--- OOPs/basics.java
+++ OOPs/basics.java
@@ -1,5 +1,16 @@
-package DSA_Java.OOPs;
-
+import java.util.*;
 public class basics {
-    
+    public static class Student {
+            String name;
+            int RollNo;
+            double percentage;
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+             s1.name="Shubham";
+             s1.RollNo=32;
+             s1.percentage=94.3; 
+             System.out.println(s1.RollNo);
+    }
 }
+
