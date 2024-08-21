b478faae2a086735f82b12ff5cd373eab1721851
Shubham-0004 <itsmeshubham2904@gmail.com>
2024-08-21 23:34:40 +0530

    Length of the String

--- Recursions-2/Length.java
+++ Recursions-2/Length.java
@@ -11,7 +11,7 @@ public class Length {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter any word:");
+        System.out.println("Enter anything:");
         String str=sc.nextLine();
 
         int output =chkL(str,0);
